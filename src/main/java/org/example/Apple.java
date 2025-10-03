f6a7ecafda7bc502e14545c9dfe4e88c174c5b4f
bohdan2353 <bohdan.mysliavchuk@lernende.bbw.ch>
2025-10-03 13:15:32 +0200

    Start

---
+++ src/main/java/org/example/Apple.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Apple {
+    private int x;
+    private int y;
+
+    public World(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void getPosition() {
+        return
+    }
+}
---
+++ src/main/java/org/example/World.java
@@ -0,0 +1,26 @@
+package org.example;
+
+public class World {
+    private Snake snake;
+    private List<Apple> apples = new ArrayList<>();
+    private int width;
+    private int height;
+
+    public World(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public void setWorldSize(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    private void addApple(int x, int y) {
+        apples
+    }
+
+    private void addApple() {
+
+    }
+}
